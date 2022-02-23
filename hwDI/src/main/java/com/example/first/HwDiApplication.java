19991a2f67c7a45d4ecd4a3c9d31435ec125ba34
starforce21 <jackjack1214@gmail.com>
2022-02-23 10:35:55 -0500

    hw

---
+++ hwDI/src/main/java/com/example/first/HwDiApplication.java
@@ -0,0 +1,13 @@
+package com.example.first;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HwDiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HwDiApplication.class, args);
+	}
+
+}
---
+++ hwDI/src/main/java/com/example/first/Laptop.java
@@ -0,0 +1,52 @@
+package com.example.first;
+
+public class Laptop {
+	
+	private int price;
+	private int ram;
+	private int ssd;
+	private double weight;
+	
+	public Laptop() {}
+
+//	public String getName() {
+//		return name;
+//	}
+//
+//	public void setName(String name) {
+//		this.name = name;
+//	}
+
+	public int getPrice() {
+		return price;
+	}
+
+	public void setPrice(int price) {
+		this.price = price;
+	}
+
+	public int getRam() {
+		return ram;
+	}
+
+	public void setRam(int ram) {
+		this.ram = ram;
+	}
+
+	public int getSsd() {
+		return ssd;
+	}
+
+	public void setSsd(int ssd) {
+		this.ssd = ssd;
+	}
+
+	public double getWeight() {
+		return weight;
+	}
+
+	public void setWeight(double weight) {
+		this.weight = weight;
+	}
+	
+}
